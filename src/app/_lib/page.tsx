3d787395665734449541406698ae9186196c5e34
Brian Okoth <brianoko2021@gmail.com>
2025-03-01 15:22:00 +0300

    Add authentication and product pages with basic layouts and metadata

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,4 @@
+export default function PrivateRoute() {
+    return <h1>Private Route: You cannot view this in the browser</h1>;
+    <p>use %5F if you want _ in your URL</p>
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <>
+            <h1>My Products</h1>
+            
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+            
+        </>
+    );
+}
