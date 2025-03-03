8083f2c91cc694685df4cf4333bd64fcc8293391
Brian Okoth <brianoko2021@gmail.com>
2025-03-03 14:15:32 +0300

    Add DaisyUI plugin, enhance blog page with image, and update order product buttons

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,3 +1,18 @@
+import Image from 'next/image';
+import brayo from '@/app/brayo.jpg'
+
 export default function FirstBlog() {
-    return <h1>First Blog Post</h1>;
+    return (
+        <div className='p-8'>
+            <h1>First Blog Post</h1>
+            <Image
+            src={brayo} 
+            alt="Brayo" 
+            width={500} 
+            height={500} 
+            className='rounded-full'
+            />
+        </div>
+    )
+
 }
--- src/app/order-product/page.tsx
+++ src/app/order-product/page.tsx
@@ -15,6 +15,15 @@ export default function OrderProduct() {
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>
             Order Now
             </button>
+
+            <button className="btn btn-dash">Default</button>
+<button className="btn btn-dash btn-primary">Primary</button>
+<button className="btn btn-dash btn-secondary">Secondary</button>
+<button className="btn btn-dash btn-accent">Accent</button>
+<button className="btn btn-dash btn-info">Info</button>
+<button className="btn btn-dash btn-success">Success</button>
+<button className="btn btn-dash btn-warning">Warning</button>
+<button className="btn btn-dash btn-error">Error</button>
         </>
     );
 }
