4c53dc49e3a96284949b8589430f026a23826740
Brian Okoth <brianoko2021@gmail.com>
2025-03-20 23:06:49 +0300

    Add error handling to product reviews and implement error boundary component

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,15 +1,30 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number){
+    return Math.floor(Math.random() * count);
+}
+
 export default async function ProductReview ({params}:
     {
-        params: Promise<{productId: string; reviewId: string}>;
+        params: { productId: string; reviewId: string };
     }
 )
-
 {
-    const {productId, reviewId} = await params;
-    if(parseInt(reviewId) > 1000) {
-        notFound();
+        // Adjust the probability of throwing an error to 25%
+        const random = getRandomInt(2);
+        if (random === 1) {
+            throw new Error("Error loading review");
+        }
+    
+        const {productId, reviewId} = params;
+    
+        if(parseInt(reviewId) > 1000) {
+            notFound();
+        }
+        return (
+            <div style={{ padding: "20px", border: "1px solid #ccc", borderRadius: "5px" }}>
+                <h1>Review {reviewId} of product {productId}</h1>
+                <p>This is a detailed review of the product. Please read through the review to get more insights.</p>
+            </div>
+        );
     }
-    return <h1>Review {reviewId} of product {productId}</h1>;
-}
