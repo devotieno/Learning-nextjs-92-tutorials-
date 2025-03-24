b51dbd2f07feb0a0aa0cff53dcc84acfb3fb352b
Brian Okoth <brianoko2021@gmail.com>
2025-03-24 20:46:41 +0300

    Add comments data and API routes for fetching and posting comments

---
+++ src/app/comments/data.ts
@@ -0,0 +1,16 @@
+export const comments = [
+  { 
+    id: 1, 
+    text: "Hello, World!",
+},
+  { 
+    id: 2, 
+    text: "Hello, World!",
+ },
+    { 
+        id: 3, 
+        text: "Hello, World!",
+    },
+    
+    
+];
